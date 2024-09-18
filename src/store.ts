f9ddef68a7c32f96c8cc429100a2e6e29003c78d
Luciano Ramello <luchosr@gmail.com>
2024-09-18 14:59:16 +0200

    refactor: imported getCryptos function

--- src/store.ts
+++ src/store.ts
@@ -1,25 +1,12 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
-import axios from 'axios';
-import { CryptoCurrenciesResponseSchema } from './schema/crypto-schema';
 import { Cryptocurrency } from './types';
+import { getCryptos } from '../services/CryptoService';
 
 type CryptoStore = {
   cryptocurrencies: Cryptocurrency[];
   fetchCryptos: () => Promise<void>;
 };
-async function getCryptos() {
-  const url =
-    'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
-
-  const {
-    data: { Data },
-  } = await axios.get(url);
-  const result = CryptoCurrenciesResponseSchema.safeParse(Data);
-  if (result.success) {
-    return result.data;
-  }
-}
 
 export const useCryptoStore = create<CryptoStore>()(
   devtools((set) => ({
