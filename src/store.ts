fc036858834782e8d70bd79172920d1d18414c3a
Luciano Ramello <luchosr@gmail.com>
2024-09-18 14:44:19 +0200

    feat: implements CryptoStore type

--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,12 @@
 import { create } from 'zustand';
 import axios from 'axios';
 import { CryptoCurrenciesResponseSchema } from './schema/crypto-schema';
+import { Cryptocurrency } from './types';
 
+type CryptoStore = {
+  cryptocurrencies: Cryptocurrency[];
+  fetchCryptos: () => Promise<void>;
+};
 async function getCryptos() {
   const url =
     'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
@@ -10,11 +15,17 @@ async function getCryptos() {
     data: { Data },
   } = await axios.get(url);
   const result = CryptoCurrenciesResponseSchema.safeParse(Data);
-  console.log(result);
+  if (result.success) {
+    return result.data;
+  }
 }
 
-export const useCryptoStore = create(() => ({
-  fetchCryptos: () => {
-    getCryptos();
+export const useCryptoStore = create<CryptoStore>((set) => ({
+  cryptocurrencies: [],
+  fetchCryptos: async () => {
+    const cryptocurrencies = await getCryptos();
+    set(() => ({
+      cryptocurrencies,
+    }));
   },
 }));
