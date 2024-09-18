823ac61c18ccb3a5da257d73c79e0f7a8b7eb284
Luciano Ramello <luchosr@gmail.com>
2024-09-18 16:45:41 +0200

    feat: creates Pair type

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,10 @@
 import {
   CurrencySchema,
   CryptoCurrencyResponseSchema,
+  PairSchema,
 } from './../schema/crypto-schema';
 import { z } from 'zod';
 
 export type Currency = z.infer<typeof CurrencySchema>;
 export type Cryptocurrency = z.infer<typeof CryptoCurrencyResponseSchema>;
+export type Pair = z.infer<typeof PairSchema>;
