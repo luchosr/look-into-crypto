dad4579784a1c14c456cd10ab8a82325f633bbb3
Luciano Ramello <luchosr@gmail.com>
2024-09-18 11:57:44 +0200

    feat: adds crypto search form to app's layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import CryptoSearchForm from './components/CryptoSearchForm';
+
 function App() {
   return (
     <>
@@ -5,7 +7,9 @@ function App() {
         <h1 className="app-title">
           Cotizador de <span>Criptomonedas</span>
         </h1>
-        <div className="content"></div>
+        <div className="content">
+          <CryptoSearchForm />
+        </div>
       </div>
     </>
   );
