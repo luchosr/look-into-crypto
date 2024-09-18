1161f4d7630ed667c25f91f607739a4bb76ed9bb
Luciano Ramello <luchosr@gmail.com>
2024-09-18 14:58:30 +0200

    refactor: moved getCryptos function to services folder

---
+++ services/CryptoService.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { CryptoCurrenciesResponseSchema } from './../src/schema/crypto-schema';
+export async function getCryptos() {
+  const url =
+    'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
+
+  const {
+    data: { Data },
+  } = await axios.get(url);
+  const result = CryptoCurrenciesResponseSchema.safeParse(Data);
+  if (result.success) {
+    return result.data;
+  }
+}
