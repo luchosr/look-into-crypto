043d960afdfd8bfec431bc2f43ecf14845256279
Luciano Ramello <luchosr@gmail.com>
2024-09-19 10:27:15 +0200

    feat: improves currency fetching

--- services/CryptoService.ts
+++ services/CryptoService.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { CryptoCurrenciesResponseSchema } from './../src/schema/crypto-schema';
+import { Pair } from '../src/types';
 export async function getCryptos() {
   const url =
     'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD';
@@ -11,4 +12,12 @@ export async function getCryptos() {
   if (result.success) {
     return result.data;
   }
+}
+
+export async function fetchCurrentCryptoPrice(pair: Pair) {
+  const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${pair.cryptocurrency}&tsyms=${pair.currency}`;
+  const {
+    data: { DISPLAY },
+  } = await axios.get(url);
+  console.log(DISPLAY[pair.cryptocurrency][pair.currency].PRICE);
 }
